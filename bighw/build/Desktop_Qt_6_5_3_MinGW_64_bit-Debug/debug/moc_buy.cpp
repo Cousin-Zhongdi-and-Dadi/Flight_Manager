fd330c20b5dd65c52b7e3a81628fd7ad78c2d7eb
Cousin-Zhongdi-and-Dadi <3258372254@qq.com>
2025-06-15 23:41:39 +0800

    Fix for issue #1

---
+++ bighw/build/Desktop_Qt_6_5_3_MinGW_64_bit-Debug/debug/moc_buy.cpp
@@ -0,0 +1,126 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'buy.h'
+**
+** Created by: The Qt Meta Object Compiler version 69 (Qt 6.9.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../../buy.h"
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'buy.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 69
+#error "This file was generated using the moc from 6.9.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+struct qt_meta_tag_ZN16Customor_DingPaoE_t {};
+} // unnamed namespace
+
+template <> constexpr inline auto Customor_DingPao::qt_create_metaobjectdata<qt_meta_tag_ZN16Customor_DingPaoE_t>()
+{
+    namespace QMC = QtMocConstants;
+    QtMocHelpers::StringRefStorage qt_stringData {
+        "Customor_DingPao",
+        "customor_dingpao_closed",
+        "",
+        "on_pushButtonBack_clicked",
+        "on_pushButton_clicked"
+    };
+
+    QtMocHelpers::UintData qt_methods {
+        // Signal 'customor_dingpao_closed'
+        QtMocHelpers::SignalData<void()>(1, 2, QMC::AccessPublic, QMetaType::Void),
+        // Slot 'on_pushButtonBack_clicked'
+        QtMocHelpers::SlotData<void()>(3, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'on_pushButton_clicked'
+        QtMocHelpers::SlotData<void()>(4, 2, QMC::AccessPrivate, QMetaType::Void),
+    };
+    QtMocHelpers::UintData qt_properties {
+    };
+    QtMocHelpers::UintData qt_enums {
+    };
+    return QtMocHelpers::metaObjectData<Customor_DingPao, qt_meta_tag_ZN16Customor_DingPaoE_t>(QMC::MetaObjectFlag{}, qt_stringData,
+            qt_methods, qt_properties, qt_enums);
+}
+Q_CONSTINIT const QMetaObject Customor_DingPao::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_staticMetaObjectStaticContent<qt_meta_tag_ZN16Customor_DingPaoE_t>.stringdata,
+    qt_staticMetaObjectStaticContent<qt_meta_tag_ZN16Customor_DingPaoE_t>.data,
+    qt_static_metacall,
+    nullptr,
+    qt_staticMetaObjectRelocatingContent<qt_meta_tag_ZN16Customor_DingPaoE_t>.metaTypes,
+    nullptr
+} };
+
+void Customor_DingPao::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    auto *_t = static_cast<Customor_DingPao *>(_o);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        switch (_id) {
+        case 0: _t->customor_dingpao_closed(); break;
+        case 1: _t->on_pushButtonBack_clicked(); break;
+        case 2: _t->on_pushButton_clicked(); break;
+        default: ;
+        }
+    }
+    if (_c == QMetaObject::IndexOfMethod) {
+        if (QtMocHelpers::indexOfMethod<void (Customor_DingPao::*)()>(_a, &Customor_DingPao::customor_dingpao_closed, 0))
+            return;
+    }
+}
+
+const QMetaObject *Customor_DingPao::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Customor_DingPao::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_staticMetaObjectStaticContent<qt_meta_tag_ZN16Customor_DingPaoE_t>.strings))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int Customor_DingPao::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    }
+    if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 3)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 3;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void Customor_DingPao::customor_dingpao_closed()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+QT_WARNING_POP
